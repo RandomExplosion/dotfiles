d50117aa7ebfb29531757a4569c5b42863c88c80
RandomExplosion <benjaminjmcgaw@gmail.com>
2022-04-22 11:41:22 +1000

    Add confirmation prompt to install script, Make install script remove conflicting files before linking

--- init-dotfiles.sh
+++ init-dotfiles.sh
@@ -1,9 +1,31 @@
 echo Targeting Directory $1
-for filename in * 
-do
-    if [ $filename != $0 ]; then
-    echo $filename ...
-    echo Linking $PWD/$filename to $1
-    ln -s $PWD/$filename $1
-    fi
-done
+echo WARNING: This will overwrite conflicting config files in the target directory. Make sure you have made a backup before doing this.
+
+# Prompt for confirmation
+read -r -p "Are you sure you wish to continue? [y/N] " input
+case $input in
+    # Yes
+    [yY][eE][sS]|[yY])
+    echo Proceeding...
+    for filename in *    
+    do
+        if [ $filename != $0 ]; then
+            echo $filename ...
+            echo Linking $PWD/$filename to $1
+	    rm -rf $1/$filename
+	    ln -s $PWD/$filename $1/$filename
+        fi
+    done
+    ;;
+    
+    # No (default)
+    [nN][oO]|[nN]|"")
+    echo "Aborting..."
+    ;;
+    
+    # Invalid
+    *)
+    echo "Invalid input. Aborting"
+    exit 1
+    ;;
+esac
