5b3911e90879c9eacc9a7904e30bf264d24806eb
RandomExplosion <benjaminjmcgaw@gmail.com>
2023-03-22 11:00:15 +1100

    Backlight configuration

--- eww/bars/scripts/nit_state.zsh
+++ eww/bars/scripts/nit_state.zsh
@@ -1,10 +1,11 @@
 #!/usr/bin/env zsh
 
 monitor="/sys/class/backlight/intel_backlight/actual_brightness"
+max="/sys/class/backlight/intel_backlight/max_brightness"
 nit_current=$(<"$monitor")
 
 function nit_percent () {
-    echo $((nit_current * 100 / 120000))
+    echo $((nit_current / max_brightness) * 100)
 }
 
 nit_percent
