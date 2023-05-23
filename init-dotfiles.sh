ece66292e1e6dbb71dc85f2b38215a0f81287b6b
RandomExplosion <benjaminjmcgaw@gmail.com>
2023-05-23 11:06:03 +1000

    Update initialisation script to use xdg's config directory

--- init-dotfiles.sh
+++ init-dotfiles.sh
@@ -1,8 +1,16 @@
-echo Targeting Directory $1...
+if [ $1 == '']; then
+    echo No Target Directory Given!
+    dest=$XDG_CONFIG_DIR
+else 
+    dest=$1
+fi
+
+echo Targeting Directory $dest
 echo  
 echo WARNING: This will overwrite conflicting config files in the target directory.
 echo Make sure you have made a backup before doing this.
 echo 
+
 # Prompt for confirmation
 read -r -p "Are you sure you wish to continue? [y/N] " input
 case $input in
@@ -13,9 +21,9 @@ case $input in
     do
         if [ $filename != $0 ]; then
             echo $filename ...
-            echo Linking $PWD/$filename to $1
-	    rm -rf $1/$filename
-	    ln -s $PWD/$filename $1/$filename
+            echo Linking $PWD/$filename to $dest
+	    rm -rf $dest/$filename
+	    ln -s $PWD/$filename $dest/$filename
         fi
     done
     ;;
